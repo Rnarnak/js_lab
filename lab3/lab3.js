beecfa7c06f458e511496899885dd0111de28ffb
Rnarnak <ivankokulokov@gmail.com>
2026-06-04 18:46:04 +0300

    lab3.js

--- lab3/lab3.js
+++ lab3/lab3.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import { fib } from '../lab3/lab2_module.js';
+import { fib } from '../lab3/lab2.js';
 /**
  * Возвращает дробную часть числа
  * @param {number} num - Исходное число
